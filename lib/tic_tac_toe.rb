f3c987ebf5330e347a92abba745fc409d7a7761a
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-03 23:34:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,21 @@
+
 class TicTacToe
 
-   WIN_COMBINATIONS = [
-     [0, 1, 2],
-     [3, 4, 5],
-     [6, 7, 8],
-     [0, 3, 6],
-     [1, 4, 7],
-     [2, 5, 8],
-     [0, 4, 8],
-     [6, 4, 2]
-   ]
-  def initialize
-    @board = Array.new(9, " ")
-  end
+ WIN_COMBINATIONS = [
+   
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [6, 4, 2]
+ ]
+ 
+def initialize
+ @board = Array.new(9, " ")
+end
 
 def display_board
  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -23,114 +26,119 @@ def display_board
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
+ user_input.to_i - 1
 end
 
 def move(index, current_player = "X")
-  @board[index] = current_player
+ @board[index] = current_player
 end
 
 def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+ !(@board[index].nil? || @board[index] == " ")
 end
 
 def valid_move?(index)
-  index.between(0,8) && !position_taken?(index)
-end
-
-def turn
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(index)
-    player = current_player
-    move(index, player)
-    display_board
-  else
-    turn
-  end
+ index.between?(0,8) && !position_taken?(index)
 end
 
 def turn_count
-  turn = 0
-  @board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
-  end
-  return turn
+ turn = 0
+ @board.each do |index|
+   if index == "X" || index == "O"
+     turn += 1
+   end
+ end
+ return turn
 end
 
 def current_player
-  turn_num = turn_count
-  if turn_num % 2 == 0
-    player = "X"
-  else
-    player = "O"
-  end
-  return player
+ num_turns = turn_count
+ if num_turns % 2 == 0
+   player = "X"
+ else
+   player = "O"
+ end
+ return player
+end
+
+def turn
+ puts "Please choose a number 1-9:"
+
+ user_input = gets.chomp
+ index = input_to_index(user_input)
+ 
+ if valid_move?(index)
+   player_token = current_player
+   move(index, player_token)
+   display_board
+ else
+   turn
+ end
 end
 
 def won?
-  WIN_COMBINATIONS.each {|combo|
-    index_0 = combo[0]
-    index_1 = combo[1]
-    index_2 = combo[2]
 
-    position_1 = @board[index_0]
-    position_2 = @board[index_1]
-    position_3 = @board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return combo
-    end
-  }
-  return false
+ WIN_COMBINATIONS.each {|win_combo|
+   index_0 = win_combo[0]
+   index_1 = win_combo[1]
+   index_2 = win_combo[2]
+   position_1 = @board[index_0]
+   position_2 = @board[index_1]
+   position_3 = @board[index_2]
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     return win_combo
+   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_combo
+   end
+ }
+ return false
 end
 
 def full?
-  @board.all? {|index| index == "X" || index ==  "O"}
+ @board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?
-  if !won? && full?
-    return true
-  else
-    return false
-  end
+ if !won? && full?
+   return true
+ else
+   return false
+ end
 end
 
 def over?
-  if won? || draw?
-    return true
-  else
-    return false
-  end
+ if won? || draw?
+   return true
+ else
+   return false
+
+ end
 end
 
-def winner?
-  if won?
-    @board[won?[0]]
-  else
-    nil
-  end
+def winner
+ index = []
+ index = won?
+ if index == false
+   return nil
+ else
+   if @board[index[0]] == "X"
+     return "X"
+   else
+     return "O"
+   end
+ end
 end
 
 def play
-  puts "Welcome to TicTacToe!"
-  display_board
+ until over? == true
+   turn
+ end
 
-  until over? do
-    turn
-  end
-
-  if won?
-    puts "Congratulations #{winner}!"
-  else draw?
-    puts "Cat's Game!"
-  end
+ if won?
+   puts "Congratulations #{winner}!"
+ elsif draw?
+   puts "Cat's Game!"
+ end
 end
 
-end # Class closing tag
+end 
